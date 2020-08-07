149b8e5ec7188cb0574599c4e195daa4ca297524
Krittawat Wisedsuwon <krittawat.w@ngerntidlor.com>
2020-08-07 13:35:43 +0700

    first commit without jaxb

---
+++ src/main/java/kriwis/springframework/kriwisbreweryservice/KriwisBreweryServiceApplication.java
@@ -0,0 +1,13 @@
+package kriwis.springframework.kriwisbreweryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KriwisBreweryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KriwisBreweryServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kriwis/springframework/kriwisbreweryservice/KriwisBreweryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package kriwis.springframework.kriwisbreweryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KriwisBreweryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
